5b1f819261f9a1c907dd735a3202af9017de4726
Vernet Hadrien <hadrien.vernet@student-cs.fr>
2025-08-25 18:46:45 +0200

    CAC40.csv + MV-constraints comparison

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Portfolio-Optimisation
+
+This folder contains different files that all compare different apsect of portfolio optimization. 
+
+- cac40_download.py is just downloads the closure price of all the stocks in the CAC40 (the top 40 french firms with biggest capitalisation) from yahoo finance starting from January 1st 2022 to July 1st 2025. Then it saves them in cac40.csv
+
+- MV_constraints.ipynb aims to compare different (basic) constraints in portfolio optimisaion, based on their performances: allocation diversity, return, Sharpe ratio. We will focus on the long-only, L/S and leverage constaints with a simple MV optimisation problem. Secondly we add a risk aversion parameter to the Markowitz problem simuation. We find the best value of it over the first semester of 2025.
---
+++ cac40_download.py
@@ -0,0 +1,48 @@
+import yfinance as yf
+
+cac40_tickers = [
+    "ACA.PA",   # Crédit Agricole
+    "AC.PA",    # Accor
+    "AIR.PA",   # Airbus
+    "AI.PA",    # Air Liquide
+    "BNP.PA",   # BNP Paribas
+    "BN.PA",    # Danone
+    "BVI.PA",   # Bureau Veritas
+    "CAP.PA",   # Capgemini
+    "CA.PA",    # Carrefour
+    "CS.PA",    # Axa
+    "DG.PA",    # Vinci
+    "DSY.PA",   # Dassault Systèmes
+    "EDEN.PA",  # Edenred
+    "EL.PA",    # EssilorLuxottica
+    "ENGI.PA",  # Engie
+    "EN.PA",    # Bouygues
+    "ERF.PA",   # Eurofins Scientific
+    "GLE.PA",   # Société Générale
+    "KER.PA",   # Kering
+    "LR.PA",    # Legrand
+    "MC.PA",    # LVMH
+    "ML.PA",    # Michelin
+    "MT.AS",    # ArcelorMittal
+    "ORA.PA",   # Orange
+    "OR.PA",    # L'Oreal
+    "PUB.PA",   # Publicis Groupe
+    "RMS.PA",   # Hermès
+    "RNO.PA",   # Renault
+    "RI.PA",    # Pernod Ricard
+    "SAN.PA",   # Sanofi
+    "SAF.PA",   # Safran
+    "SGO.PA",   # Saint-Gobain
+    "SU.PA",    # Schneider Electric
+    "STMPA.PA", # STMicroelectronics
+    "STLAP.PA", # Stellantis
+    "TEP.PA",   # Teleperformance
+    "TTE.PA",   # TotalEnergies
+    "HO.PA",    # Thales
+    "URW.PA",   # Unibail-Rodamco-Westfield
+    "VIE.PA",   # Veolia
+]
+
+cac40 = yf.download(cac40_tickers, start="2022-01-01", end="2025-07-01", auto_adjust=True,progress=False)
+cac40_prices = cac40["Close"]
+cac40_prices.to_csv("cac40.csv")
